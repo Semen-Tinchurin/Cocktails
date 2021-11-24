beb9f41721b867158148327fd0c77586f60e6b4c
Sam <semen.tinchurin@mail.ru>
2021-11-24 13:36:55 +0300

    second commit main function added

--- some_tests.py
+++ some_tests.py
@@ -1,16 +1,25 @@
+#!/usr/bin/python3
 import json
 
-print("Поиск:")
-print("По названию коктейля [1]")
-print("По основе коктейля [2]")
-print("По основе вместе с рецептами [3]")
-print("Выход [0]")
 
-choice = int(input("Ввод:>>> "))
+def __main__():
+    print("Поиск:")
+    print("По названию коктейля [1]")
+    print("По основе вместе с рецептами [2] ")
+    print("По основе коктейля [3] ")
+    print("Выход [0]")
 
-with open("cocktails_recipes_dict.json", 'r', encoding='windows-1251') as file:
-    file = file.read()
-    file = json.loads(file)
+    # python3 -m cocktails -n "name" | -b "base" | -B "cocktail base"
+    # ((?:[А-Яа-я]+\s+)+)(\d+)([А-Яа-я]+)
+
+    choice = int(input("Ввод:>>> "))
+
+    file = None
+
+    with open("cocktails_recipes_dict.json", 'r', encoding='windows-1251') as json_file:
+        file = json_file.read()
+        file = json.loads(file)
+
     while 1 <= choice <= 3:
         if choice == 1:
             query = input('Введите название коктейля: ')
@@ -24,30 +33,31 @@ with open("cocktails_recipes_dict.json", 'r', encoding='windows-1251') as file:
             ingredient = input('Введите основу: ')
             for name_of_cocktail, list_ingredients in file.items():
                 num = 0
-                for item in list_ingredients[0]:
-                    if num == 0:
-                        if ingredient.lower() in item:
-                            print(name_of_cocktail)
-                            num += 1
-                    else:
-                        break
+                if num == 0:
+                    if ingredient.lower() in list_ingredients[0][0]:
+                        print(name_of_cocktail, ':')
+                        for item2 in list_ingredients[0]:
+                            print(item2)
+                        print("--------------------------------------------------")
+                        num += 1
+                else:
+                    break
 
         elif choice == 3:
             ingredient = input('Введите основу: ')
             for name_of_cocktail, list_ingredients in file.items():
                 num = 0
-                for item in list_ingredients[0]:
-                    if num == 0:
-                        if ingredient.lower() in item:
-                            print(name_of_cocktail, ':')
-                            for item2 in list_ingredients[0]:
-                                print(item2)
-                            print("--------")
-                            num += 1
-                    else:
-                        break
+                if num == 0:
+                    if ingredient.lower() in list_ingredients[0][0]:
+                        print(name_of_cocktail)
+                        num += 1
+                else:
+                    break
 
         else:
             print('error')
 
         choice = int(input("Ввод:>>> "))
+
+
+__main__()
